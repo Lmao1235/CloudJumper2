ee7a7f5b36e9b6b8564a45010eeb527f914da8d1
Lmao1235 <148939689+Lmao1235@users.noreply.github.com>
2024-03-21 04:35:32 +0700

    movement

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
+    [SerializeField] private float speed;
+
+    Vector3 movement;
+
+    float HorizonInput;
+    float VertaicalInput;
+
+
+    public Rigidbody rb;
+
+   
+
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+
         
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * VertaicalInput);
+        //transform.Translate(-Vector3.forward * Time.deltaTime * HorizonInput);
+        transform.Rotate(Vector3.up * HorizonInput * speed * Time.deltaTime);
+
+
     }
 }
